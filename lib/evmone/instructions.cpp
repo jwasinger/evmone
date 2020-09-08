761d20b5fc2779fd8980cff37094ff04efef24c4
Jared Wasinger <j-wasinger@hotmail.com>
2020-09-08 14:23:28 -0700

    add memory check

--- lib/evmone/instructions.cpp
+++ lib/evmone/instructions.cpp
@@ -1252,6 +1252,9 @@ const instruction* op_addmod384(const instruction* instr, execution_state& state
     const auto y = &state.memory[static_cast<size_t>(y_offset) * 48];
     const auto m = &state.memory[12288];
 
+    if (!check_memory(state, 12288, 48))
+        return nullptr;
+
     addmod384_64bitlimbs(
         reinterpret_cast<uint64_t*>(out),
         reinterpret_cast<uint64_t*>(x),
@@ -1273,6 +1276,9 @@ const instruction* op_submod384(const instruction* instr, execution_state& state
     const auto x = &state.memory[static_cast<size_t>(x_offset) * 48];
     const auto y = &state.memory[static_cast<size_t>(y_offset) * 48];
     const auto m = &state.memory[12288];
+
+    if (!check_memory(state, 12288, 48))
+        return nullptr;
 
     subtractmod384_64bitlimbs(
         reinterpret_cast<uint64_t*>(out),
@@ -1296,6 +1302,9 @@ const instruction* op_mulmodmont384(const instruction* instr, execution_state& s
     const auto y = &state.memory[static_cast<size_t>(y_offset) * 48];
     const auto m = &state.memory[12288];
     const uint64_t *inv = reinterpret_cast<uint64_t *>(state.memory[12336]);
+
+    if (!check_memory(state, 12336, 48))
+        return nullptr;
 
     montmul384_64bitlimbs(
         reinterpret_cast<uint64_t*>(out),
